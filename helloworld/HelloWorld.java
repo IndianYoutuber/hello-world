3e64a0b85159a8afd4953efb546ce66a125cf7ed
IndianYoutuber <43661906+IndianYoutuber@users.noreply.github.com>
2019-03-27 12:11:33 +0800

    Update HelloWorld.java

--- helloworld/HelloWorld.java
+++ helloworld/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("bnnmjyy tjn yt2nhju47 27536");
+        System.out.println("Hello World");
         
     }
     
